0a381edf596d56386aa8888a2cc9ddd552cf6723
sushoucai <sushoucai@git.mistong.com>
2018-01-06 17:59:04 +0800

    modified api details component.

--- mockingjayApp/src/components/ApiInventory/ApiDetails.vue
+++ mockingjayApp/src/components/ApiInventory/ApiDetails.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>{{ JSON.stringify(details) }}</div>
+  <form id="fm_details">
+    <div id="pn_details">
+      <label for="ipt_path">Path: </label>
+      <input id="ipt_path" v-model="details.path" v-on:change="validPath" ><br/>
+      <label for="ipt_method">Method: </label>
+      <input id="ipt_method" v-model="details.method" v-on:change="validPath" ><br/>
+      <label for="ck_mock">Mock: </label>
+      <input type="check" id="ck_mock" v-model="details.mock" v-on:change="validPath" ><br/>
+    </div>
+  </form>
 </template>
 
 <script>
-const queryString = require("querystring");
-
 export default {
   name: "ApiList",
   data() {
@@ -13,12 +20,21 @@ export default {
     };
   },
   mounted: function() {
-      var dataStr = this.$route.query.data;
-      this.details = JSON.parse(dataStr)
+    var dataStr = this.$route.query.data;
+    this.details = JSON.parse(dataStr);
+  },
+  methods: {
+    validPath: function() {
+      console.log(this.details.path);
+    }
   }
 };
 </script>
 
 <style>
 
+#pn_details {
+  margin: 80px 0 80px 200px;
+  text-align: left;
+}
 </style>
