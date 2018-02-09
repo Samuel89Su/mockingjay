3079f20190c93062c064e44ce39d1af1fb06f5bf
sushoucai <sushoucai@git.mistong.com>
2018-02-09 09:14:32 +0800

    remove manager.js

--- managerbyreact/src/index.js
+++ managerbyreact/src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Manager from './components/manager.js';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import managerApp from './reducers'
+import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom'
+import { appListC, AppRoutes } from '../src/components/appListC'
 
 const store = createStore(managerApp, applyMiddleware(thunk))
 
+class PageHeader extends Component {
+    render() {
+        return (
+            <div className='page-header'>
+                <h1>{ this.props.title }</h1>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
 	(<Provider store={store}>
-		<Manager store={store} />
+		<Router history={browserHistory}>
+			<div>
+                <PageHeader title='Mockingjay'></PageHeader>
+            
+                <Route exact path='/' component={appListC}></Route>
+                {
+                    AppRoutes.map((route, index) => {
+                        return <Route key={index} path={route.path} component={route.component} ></Route>
+                    })
+                }
+            </div>
+		</Router>
 	</Provider>),
 	document.getElementById('root')
-);
+)
--- mockingjay/router.js
+++ mockingjay/router.js
@@ -1,22 +1,24 @@
 'use strict'
 
 const Router = require('koa-router')
+const send = require('koa-send')
 const brokerRouter = require('./src/components/broker/router')
 const inventoryRouter = require('./src/components/inventory/router')
 
 const router = new Router()
-
-router.use('/', async (ctx, next) => {
-    await next()
-    if (ctx.status !== 200) {
-        console.log(ctx.path + ':' + ctx.status)
-    }
-})
 
 // register mocking module routes
 router.use('/mocking', brokerRouter.routes(), brokerRouter.allowedMethods())
 
 // register api inventory module routes
 router.use('/inventory', inventoryRouter.routes(), inventoryRouter.allowedMethods())
+
+router.get(/^\/(app|api)/i, async (ctx, next) => {
+    // filter resource file
+    if (ctx.path) {
+        await next()
+    }
+    await send(ctx, 'index.html', { root: __dirname + '/static' })
+})
 
 exports = module.exports = router
