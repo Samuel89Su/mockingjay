aaa93ba05f00f17d725d20b4d005a9d76f76e825
sushoucai <sushoucai@git.mistong.com>
2018-02-09 09:34:35 +0800

    fix browser refresh bug.

--- mockingjay/router.js
+++ mockingjay/router.js
@@ -13,12 +13,14 @@ router.use('/mocking', brokerRouter.routes(), brokerRouter.allowedMethods())
 // register api inventory module routes
 router.use('/inventory', inventoryRouter.routes(), inventoryRouter.allowedMethods())
 
+const regexp = /.(js|css)$/i
 router.get(/^\/(app|api)/i, async (ctx, next) => {
     // filter resource file
-    if (ctx.path) {
+    if (regexp.test(ctx.path)) {
         await next()
+    } else {
+        await send(ctx, 'index.html', { root: __dirname + '/static' })
     }
-    await send(ctx, 'index.html', { root: __dirname + '/static' })
 })
 
 exports = module.exports = router
