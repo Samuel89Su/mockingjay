df1ea8c102dafdc1f8d371535fc49c8beb9aabe9
sushoucai <sushoucai@git.mistong.com>
2018-02-12 17:42:44 +0800

    add cachekey combine function

--- mockingjay/src/components/common/cacheKeys.js
+++ mockingjay/src/components/common/cacheKeys.js
@@ -9,4 +9,47 @@ const cacheKeys = {
     mockCfgPostfix: 'mockCfg'
 }
 
+const keyPrefixes = {
+    appIdIncre: 'appid',
+    apiIdIncre: 'apiId',
+    appInventory: 'appinventory_',
+    apiInventory: 'apiinventory:',
+}
+
+function CombineCacheKey(cacheType, params) {
+    if (!keyPrefixes.hasOwnProperty(cacheType)) {
+        return null
+    } else {
+        let keyPrefix = keyPrefixes[cacheType]
+        if (!params) {
+
+        } else if (typeof params !== 'object' && !(params instanceof Array)) {
+            keyPrefix += params
+        } else if (params instanceof Array) {
+            for (let i = 0; i < params.length; i++) {
+                let partial = params[i];
+                if (partial) {
+                    keyPrefix += partial + '_'
+                }
+            }
+        } else if (typeof params === 'object') {
+            for (const key in params) {
+                if (params.hasOwnProperty(key)) {
+                    const val = params[key];
+                    if (val) {
+                        keyPrefix += val + '_'
+                    }
+                }
+            }
+        } else {
+            throw new TypeError('type of params is invalid')
+        }
+
+        if (keyPrefix.endsWith('_')) {
+            keyPrefix = keyPrefix.subStr(0, keyPrefix.length - 1)
+        }
+        return keyPrefix
+    }
+}
+
 exports = module.exports = cacheKeys;
--- mockingjay/src/components/common/redisClient.js
+++ mockingjay/src/components/common/redisClient.js
@@ -15,9 +15,4 @@ const redisConOption = {
 
 const redisClient = Redis.createClient(redisConOption)
 
-// w & r test
-// const ok = await redisClient.setAsync('test', 123) === 'OK'
-// ok = ok && await redisClient.getAsync('test') === '123'
-// logger.error(`redis w & r test ${ok ? 'success' : 'failed'}`)
-
 exports = module.exports = redisClient
