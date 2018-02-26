0d7cc9ec3b0b08a1fa78fed3463187217ea6da0e
Samuel <sushoucai1989@hotmail.com>
2018-02-26 23:12:12 +0800

    update proxy-master

--- proxy-master/index.js
+++ proxy-master/index.js
@@ -4,8 +4,9 @@
 const http = require('http')
 const connect = require('connect')
 const proxy = require('http-proxy-middleware')
+const { URL } = require('url')
 const fs = require('fs')
-const serveStatic = require('serve-static')
+const serveStatic = require('./serve-static')
 const defaultOpts = require('./defaultOpts')
 const rawUserOpts = fs.readFileSync('./opts.json')
 
@@ -14,18 +15,16 @@ try {
   const context = userOpts.context
   delete userOpts.context
   const opts = Object.assign({}, userOpts, defaultOpts)
+
+  const app = connect()
+
+  const serve = serveStatic('static/home', { 'index': ['index.html'], autoCompleteHtmlExtension: true })
+  app.use('/home', serve)
   
   /**
    * Configure proxy middleware
    */
   const userProxy = proxy(context, opts)
-
-  const serve = serveStatic('static', {'index': ['index.html']})
-
-  const app = connect()
-
-  app.use(serve);
-
   /**
    * Add the proxy to connect
    */
