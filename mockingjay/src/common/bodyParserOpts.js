043a22b724a4a884d5118004b0a7160676218a6d
sushoucai <sushoucai@git.mistong.com>
2018-02-27 10:45:56 +0800

    fix bodyparser bug

--- mockingjay/src/common/bodyParserOpts.js
+++ mockingjay/src/common/bodyParserOpts.js
@@ -1,12 +1,14 @@
-'use strict';
+'use strict'
+
+const logger = require('./logger')
 
 const Opts = {
     common: {
         enableTypes: ['json', 'form', 'text'],
         onerror: function (err, ctx) {
-            logger.error(err);
+            logger.error(err)
         }
     }
-};
+}
 
-exports = module.exports = Opts;
+exports = module.exports = Opts
