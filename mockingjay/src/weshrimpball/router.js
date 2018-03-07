81f8aa15e8f60186d6e7f9a96c0c113699af0cf5
Samuel <sushoucai1989@hotmail.com>
2018-03-07 22:55:10 +0800

    add wechat sign verification

---
+++ mockingjay/src/weshrimpball/router.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Router = require('koa-router')
+const handleError = require('../common/handleError')
+const verifyWeChatSign = require('./verifySign')
+
+const router = new Router()
+
+router.use('/*', handleError)
+
+router.get('/signverify', verifyWeChatSign)
+
+exports = module.exports = router
