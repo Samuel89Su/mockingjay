45d53dd6c3d7d71a5ecb4c4f6587bbb6b8dfe907
Samuel <sushoucai1989@hotmail.com>
2018-03-18 23:44:05 +0800

    add react-router-redux examples

---
+++ examples/basic/webpack/webpack.config.dev.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Webpack = require('webpack')
+
+module.exports = {
+  output: {
+    filename: 'bundle.js',
+    // chunkFilename: '[name].js'
+  },
+  plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
+    new Webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './src',
+    inline: true,
+    hot: true,
+    port: 8100,
+    historyApiFallback: true
+  }
+}
