b3a977ac40fa5f3baeee8fa0a630f7813ddf908d
Samuel <sushoucai1989@hotmail.com>
2018-03-19 21:44:21 +0800

    update config

--- mockingjay/defaultConfig.js
+++ mockingjay/defaultConfig.js
@@ -1,8 +1,13 @@
-const config = require('./config')
+const fs = require('fs')
 
 const defaultConfig = {
     host: '0.0.0.0',
     port: 3000
+}
+
+let config = {}
+if (fs.statSync('./config.js').isFile()) {
+    config = require('./config')
 }
 
 const mergedConfig = Object.assign({}, defaultConfig, config)
