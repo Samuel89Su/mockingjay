9720f5b6172675dd85d4fcb33dd6c4472ca7fa1d
Samuel <sushoucai1989@hotmail.com>
2018-03-19 21:37:02 +0800

    revert index.js

--- manager/src/index.js
+++ manager/src/index.js
@@ -1,60 +1,38 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-
 import { Provider } from 'react-redux'
-
-import createHistory from 'history/createBrowserHistory'
-import { Route } from 'react-router'
-
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
-
-import reducers from './reducers'
-
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom'
 import Loadable from 'react-loadable'
 import { AppRoutes } from './components/AppListC'
 import { AppListC } from './components/AppListC'
 import Layout from './components/Layout'
+import reducers from './reducers'
 import '../semantic/dist/semantic.min.css'
 import Loading from './components/Loading'
-
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory()
-
-// Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
-
-// Add the reducer to your store on the `router` key
-// Also apply our middleware for navigating
-const store = createStore(
-  combineReducers({
-    ...reducers,
-    router: routerReducer
-  }),
-  applyMiddleware(thunk, middleware)
-)
 
 // const AppListC = Loadable({
 //   loader: () => import('./components/AppListC').AppListC,
 //   loading: Loading
 // })
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 ReactDOM.render(
 	(<Provider store={store}>
-        <ConnectedRouter history={history}>
+        <Router history={browserHistory}>
             <Layout>
-                <Route exact path='/' component={AppListC} />
+                <Route exact path='/' component={AppListC}></Route>
                 {
                     AppRoutes.map((route, index) => {
-                        return <Route key={index} path={route.path} component={route.component} />
+                        return <Route key={index} path={route.path} component={route.component} ></Route>
                     })
                 }
             </Layout>
-		</ConnectedRouter>
+		</Router>
 	</Provider>),
 	document.getElementById('root')
 )
