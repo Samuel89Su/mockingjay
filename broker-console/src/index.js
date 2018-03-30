201c980e972cad4fad067de3a48d69fe7bb584bb
sushoucai <sushoucai@git.mistong.com>
2018-03-30 11:51:23 +0800

    update console

--- broker-console/src/index.js
+++ broker-console/src/index.js
@@ -2,24 +2,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom'
 import ControlPanel from './components/ControlPanel'
 import Layout from './components/Layout'
-import reducers from './reducers'
 import '../semantic/dist/semantic.min.css'
 
-const store = createStore(reducers, applyMiddleware(thunk))
-
 ReactDOM.render(
-	(<Provider store={store}>
-        <Router history={browserHistory}>
-            <Layout>
-                <Route exact path='/' component={ ControlPanel }></Route>
-            </Layout>
-		</Router>
-	</Provider>),
+	(<Layout>
+        <ControlPanel />
+    </Layout>),
 	document.getElementById('root')
 )
--- broker-console/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import { proxyCfg } from './proxyCfg'
-
-export default combineReducers({
-  proxyCfg
-})
