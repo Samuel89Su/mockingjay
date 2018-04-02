0b6196ec46ed1323e5412ca620ea5cc8eb5ed018
Samuel Su <samuel@SamueldeMacBook-Pro.local>
2018-04-02 20:20:43 +0800

    use env while render view

--- exp-prac/routes/index.js
+++ exp-prac/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let env = req.app.get('env')
+  let title = 'Express on ' + env
+  res.render('index', { title: title });
 });
 
 module.exports = router;
